b4bab8225c61a12af364176f5533378c1c88278b
Andrei Shershan <mail.shershan@gmail.com>
2025-03-05 22:47:25 +0100

    refactoring

---
+++ src/KinoDev.ApiGateway.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace KinoDev.ApiGateway.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("details")]
+        public IActionResult GetUserDetails()
+        {
+            var roles = User.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value).ToList();
+            return Ok(roles);
+        }
+    }
+}
